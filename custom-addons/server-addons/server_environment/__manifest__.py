21b72e7dc92e0c7a21655073c8ca16900ab88103
Jean <anh.nh@outlook.com>
2021-11-28 23:15:05 +0700

    remove wrong version knowledge lib, adđ some more lib and refactor

---
+++ custom-addons/server-addons/server_environment/__manifest__.py
@@ -0,0 +1,20 @@
+# Copyright Wingo SA
+# Copyright 2018 Camptocamp (https://www.camptocamp.com).
+# License LGPL-3.0 or later (https://www.gnu.org/licenses/lgpl.html)
+
+{
+    "name": "server configuration environment files",
+    "version": "14.0.2.0.1",
+    "depends": ["base", "base_sparse_field"],
+    "author": "Camptocamp,Odoo Community Association (OCA)",
+    "summary": "move some configurations out of the database",
+    "website": "https://github.com/OCA/server-env",
+    "license": "LGPL-3",
+    "category": "Tools",
+    "data": [
+        "security/ir.model.access.csv",
+        "security/res_groups.xml",
+        "serv_config.xml",
+    ],
+    "installable": True,
+}
