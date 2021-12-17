c9b3f5e2f0b72df0f9228fb9cac77879e0bdbbee
Jean <anh.nh@outlook.com>
2021-12-18 06:42:30 +0700

    add server-ux

---
+++ custom-addons/server-ux/setup/_metapackage/setup.py
@@ -0,0 +1,42 @@
+import setuptools
+
+with open('VERSION.txt', 'r') as f:
+    version = f.read().strip()
+
+setuptools.setup(
+    name="odoo14-addons-oca-server-ux",
+    description="Meta package for oca-server-ux Odoo addons",
+    version=version,
+    install_requires=[
+        'odoo14-addon-barcode_action',
+        'odoo14-addon-base_cancel_confirm',
+        'odoo14-addon-base_export_manager',
+        'odoo14-addon-base_import_security_group',
+        'odoo14-addon-base_menu_visibility_restriction',
+        'odoo14-addon-base_optional_quick_create',
+        'odoo14-addon-base_revision',
+        'odoo14-addon-base_search_custom_field_filter',
+        'odoo14-addon-base_technical_features',
+        'odoo14-addon-base_tier_validation',
+        'odoo14-addon-base_tier_validation_formula',
+        'odoo14-addon-base_tier_validation_forward',
+        'odoo14-addon-base_tier_validation_server_action',
+        'odoo14-addon-chained_swapper',
+        'odoo14-addon-date_range',
+        'odoo14-addon-default_multi_user',
+        'odoo14-addon-document_quick_access',
+        'odoo14-addon-document_quick_access_folder_auto_classification',
+        'odoo14-addon-filter_multi_user',
+        'odoo14-addon-mass_editing',
+        'odoo14-addon-mass_operation_abstract',
+        'odoo14-addon-multi_step_wizard',
+        'odoo14-addon-sequence_check_digit',
+        'odoo14-addon-sequence_range_end',
+        'odoo14-addon-sequence_reset_period',
+    ],
+    classifiers=[
+        'Programming Language :: Python',
+        'Framework :: Odoo',
+        'Framework :: Odoo :: 14.0',
+    ]
+)
