ef1faacc35ed5d89c26069d6d66d057d665a64d8
Jean <anh.nh@outlook.com>
2022-01-11 05:42:29 +0700

    delete unnecessary files nad module

--- custom-addons/pos-addons/pos_ob_default_customer/models/pos_config.py
+++
@@ -1,7 +0,0 @@
-from odoo import fields, models
-
-
-class PosConfig(models.Model):
-    _inherit = 'pos.config'
-
-    default_partner_id = fields.Many2one('res.partner', string='Select Default Customer')
